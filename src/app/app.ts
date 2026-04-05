009298834009661aeb2829ca357abc67795e163c
Francivaldo Alves <francivaldo.alves@gmail.com>
2026-04-05 19:56:47 -0300

    Add  material Design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { Layout } from './core/layout/layout';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('gerenciador-financeiro');
