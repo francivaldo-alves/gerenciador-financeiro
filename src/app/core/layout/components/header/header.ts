881aaa014b6c0c83eb567fce1018bb10cedb721a
Francivaldo Alves <francivaldo.alves@gmail.com>
2026-04-05 20:22:51 -0300

    feat: cria header usando toolbar do material

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
